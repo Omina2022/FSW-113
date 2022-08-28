5443fb5c4a53b699a5a9880a4f2233c2c8dbe7a4
Omina Sharifzoda <omina.sharifzoda@bryanuniversity.edu>
2022-08-28 17:50:36 -0600

    week4

--- week4/assignments/langs/languages2.js
+++ week4/assignments/langs/languages2.js
@@ -1,17 +1,30 @@
 const lang = 'JavaScript'
 
+
 // Create an event listener for the submit button that adds all 'input' elements to 
 // a single array using the spread operator. Call the chkLang() function, passing in 
 // the array as an argument.
 
 document.querySelector("#submit").addEventListener("click", () =>{
-const nodesArray = [...document.querySelectorAll ("input")];
-console.log(nodesArray)
-chkLang(nodesArray)
+const nodesList = document.querySelectorAll ("input");
+
+chkLang(nodesList)
 })
 function chkLang(langs) {
     let result = false
-console.log(langs[0].value)
+// console.log(langs[0])
+
+for (let i = 0; i < langs.length; i++){
+
+
+
+   if (langs[i].value.toLowerCase() == lang.toLowerCase()){
+result = true 
+break;
+   }
+
+   
+}
     
 
     // use an array method to check whether the user included 'JavaScript' in their
@@ -22,5 +35,6 @@ console.log(langs[0].value)
         obj.innerText = `Congratulations!\nYou know ${lang}.`
     else
         obj.innerText = `Sorry,\nyou don't know ${lang}.`
-}
+
+    }
  
